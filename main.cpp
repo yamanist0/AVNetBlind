d3919f27ccbb223c48418a05ae02fd7c10265880
Yamanist <yaman26@proton.me>
2026-08-24 19:37:50 +0300

    debug format changes

--- main.cpp
+++ main.cpp
@@ -172,6 +172,7 @@ bool WfpSetup() {
 
 std::wstring Utf8ToWstring(const std::string& str) {
     if (str.empty()) return std::wstring();
+    std::cout << "Converting UTF8 string of size: " << str.size() << std::endl;
     int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
     std::wstring wstrTo(size_needed, 0);
     MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
